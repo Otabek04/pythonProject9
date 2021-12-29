b657119f71ba66a9124d07fac056fc869175eda2
Otabek04 <shoraqorgon@gmail.com>
2021-12-30 00:52:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+class mashina:
+    def __init__(self,nomi,rangi,narxi,probeg):
+        self.nomi=nomi
+        self.rangi=rangi
+        self.narxi=narxi
+        self.yoli=probeg
+    def way(self,yil):
+        return yil-self.probeg
+spark=mashina('spark','qora',9000,2013)
+print(self.yoli)
